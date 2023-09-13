46deb10c88cc4d3e47a2a444474cc9f4f121cd49
mipaulius <mi.paulius@gmail.com>
2023-09-13 10:01:44 +0300

    Update 2023-09-13

--- src/pages/ForEmployersPage.js
+++ src/pages/ForEmployersPage.js
@@ -1,66 +1,149 @@
-import React from 'react';
+import React, { Component } from 'react';
 import BeaverHead from '../images/beaver-head.svg';
 import BeaverHeadHover from '../images/beaver-head-hover.svg';
+import Axios from 'axios'; // Import Axios
+import './ForEmployerPage.css'; // Import the CSS file
 
-import "./ForEmployerPage.css"; // Import the CSS file
 
-const handleMouseEnter = (e) => {
-  e.currentTarget.src = BeaverHeadHover; // Change the image source on hover
-};
+class ForEmployersPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      secondName: '',
+      companyName: '',
+      phoneNumber: '',
+      email: '',
+      message: '',
+    };
+  }
 
-const handleMouseLeave = (e) => {
-  e.currentTarget.src = BeaverHead; // Change the image source on mouse leave
-};
+  handleInputChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+    console.log(this.state); // Add this line to check state updates
+  };
 
-const ForEmployersPage = () => {
+  handleSubmit = (e) => {
+    e.preventDefault(); // Prevent default form submission behavior
+
+    // Collect form data from state
+    const formData = this.state;
+
+    // Send the form data to the server
+    Axios.post('http://localhost:3001/it-hiring', formData)
+  .then((response) => {
+    // Handle a successful response from the server
+    console.log('Form submitted successfully', response.data);
+  })
+  .catch((error) => {
+    // Handle any errors from the server
+    console.error('Error submitting form', error);
+  });
+
+  };
+
+  handleMouseEnter = (e) => {
+    e.currentTarget.src = BeaverHeadHover; // Change the image source on hover
+  };
+
+  handleMouseLeave = (e) => {
+    e.currentTarget.src = BeaverHead; // Change the image source on mouse leave
+  };
+
+  render() {
     return (
-        <div className='hero-container'>
-            <div>
-                <h1 className='for-employers-h1'>Grow your tech team with Logspot!</h1>
-                <div className='ForEmployerPageHeader'>Discover the power of Logspot's tech-savvy beavers, damming up the best IT talents for your company's growth and success.</div>
-                <div className='for-employers-page-layout'>
-                <div className="ForEmployerPageForm">
-                   
-                <form action="/submit-form" method="POST">
-  <label>
-    <input type="text" name="firstName" required placeholder="Enter your first name" />
-  </label>
-  <label>
-    <input type="text" name="secondName" required placeholder="Enter your second name" />
-  </label>
-  <label>
-    <input type="text" name="companyName" required placeholder="Enter your company name" />
-  </label>
-  <label>
-    <input type="tel" name="phoneNumber" pattern="[0-9]*" required title="Numbers only" placeholder="Enter your phone number" />
-  </label>
-  <label>
-    <input type="email" name="email" required title="Please include a '@' in the email address" placeholder="Enter your email address" />
-  </label>
-  <label>
-    <textarea name="message" rows="4" required title="Please fill out this field" placeholder="Enter your message here"></textarea>
-  </label>
-  <button type="submit">Send us a message and we will contact you shortly</button>
-</form>
-
-
-                </div>
-
-                <div className='BeaverHead'>
-                <img src={BeaverHead} 
-                className='beaver-head' 
-                alt='images with beaver head smiling'
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                />
-                </div>
-                </div>
-
+      <div className='hero-container'>
+        <div>
+          <h1 className='for-employers-h1'>Grow your tech team with Logspot!</h1>
+          <div className='ForEmployerPageHeader'>
+            Discover the power of Logspot's tech-savvy beavers, damming up the best IT talents for your company's growth
+            and success.
+          </div>
+          <div className='for-employers-page-layout'>
+            <div className='ForEmployerPageForm'>
+              <form onSubmit={this.handleSubmit} action="/api/submit-form" method="POST">
+                <label>
+                  <input
+                    type='text'
+                    name='firstName'
+                    value={this.state.firstName}
+                    onChange={this.handleInputChange}
+                    required
+                    placeholder='Enter your first name'
+                  />
+                </label>
+                <label>
+                  <input
+                    type='text'
+                    name='secondName'
+                    value={this.state.secondName}
+                    onChange={this.handleInputChange}
+                    required
+                    placeholder='Enter your second name'
+                  />
+                </label>
+                <label>
+                  <input
+                    type='text'
+                    name='companyName'
+                    value={this.state.companyName}
+                    onChange={this.handleInputChange}
+                    required
+                    placeholder='Enter your company name'
+                  />
+                </label>
+                <label>
+                  <input
+                    type='tel'
+                    name='phoneNumber'
+                    value={this.state.phoneNumber}
+                    onChange={this.handleInputChange}
+                    pattern='[0-9]*'
+                    required
+                    title='Numbers only'
+                    placeholder='Enter your phone number'
+                  />
+                </label>
+                <label>
+                  <input
+                    type='email'
+                    name='email'
+                    value={this.state.email}
+                    onChange={this.handleInputChange}
+                    required
+                    title="Please include a '@' in the email address"
+                    placeholder='Enter your email address'
+                  />
+                </label>
+                <label>
+                  <textarea
+                    name='message'
+                    value={this.state.message}
+                    onChange={this.handleInputChange}
+                    rows='4'
+                    required
+                    title='Please fill out this field'
+                    placeholder='Enter your message here'
+                  ></textarea>
+                </label>
+                <button type='submit'>Send us a message and we will contact you shortly</button>
+              </form>
             </div>
 
-            
+            <div className='BeaverHead'>
+              <img
+                src={BeaverHead}
+                className='beaver-head'
+                alt='images with beaver head smiling'
+                onMouseEnter={this.handleMouseEnter}
+                onMouseLeave={this.handleMouseLeave}
+              />
+            </div>
+          </div>
         </div>
+      </div>
     );
-};
+  }
+}
 
 export default ForEmployersPage;
